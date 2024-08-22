7be333adc6762ee62319ce123260bc91ef5407b5
Biplob Dash <110329225+BiplobDash@users.noreply.github.com>
2024-08-22 18:27:53 +0600

    Setup Project

---
+++ lib/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () async {
+                  
+                },
+                child: const Text('Notification')),
+          ],
+        ),
+      ),
+    );
+  }
+}
