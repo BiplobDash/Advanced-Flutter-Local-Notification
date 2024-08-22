a8645da78f272d08b86375e0dfab11af7d34ff3b
Biplob Dash <110329225+BiplobDash@users.noreply.github.com>
2024-08-22 18:40:37 +0600

    Fill Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:advanced_local_notification/home_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp(),);
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
